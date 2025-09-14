dd26cb31331ef1bfdd166d31d6d5a90ce80caa4f
Sajad Thapa <sajadthapa04@gmail.com>
2025-09-14 15:52:55 +0545

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Sajad Thapa on 9/14/25.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
